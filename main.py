a7121e60d69f140bbd914cd0e00ac31ce8aeac6e
Ali Youseliyani <102595263+Aliyouseliyani@users.noreply.github.com>
2025-11-11 18:03:46 +0330

    week 3: strip method

--- main.py
+++ main.py
@@ -58,39 +58,59 @@ Second week
 #         print(f"{dahgan ** yekan} --- {yekan ** dahgan}")
 
 # # Practice 4: Matrix
-result = []
-rows_a = int(input("Rows number of matrix A: "))
-cols_a = int(input("columns number of matrix A: "))
+# result = []
+# rows_a = int(input("Rows number of matrix A: "))
+# cols_a = int(input("columns number of matrix A: "))
+#
+# a = []
+# print("numbers of matrix A: ")
+# for i in range(rows_a):
+#     row = list(map(int, input().split()))
+#     a.append(row)
+#
+#
+# rows_b = int(input("Rows number of matrix B: "))
+# cols_b = int(input("columns number of matrix B:  "))
+#
+# b = []
+# print("numbers of matrix B: ")
+# for i in range(rows_b):
+#     row = list(map(int, input().split()))
+#     b.append(row)
+#
+#
+# if cols_a != rows_b:
+#     print("Error: Number of columns of matrix A should be equal to number of rows of matrix B")
+# else:
+#     result = [[0 for j in range(cols_b)] for i in range(rows_a)]
+#
+# for i in range(rows_a):
+#     for j in range(cols_b):
+#         for k in range(cols_a):
+#             result[i][j] += a[i][k] * b[k][j]
+#
+# for row in result:
+#     print(row)
 
-a = []
-print("numbers of matrix A: ")
-for i in range(rows_a):
-    row = list(map(int, input().split()))
-    a.append(row)
+
+""" Third week """
 
 
-rows_b = int(input("Rows number of matrix B: "))
-cols_b = int(input("columns number of matrix B:  "))
+def strip(string):
+    index_first_letter = 0
+    index_last_letter = 0
+    for x in range(len(string)):
+        if string[-(x + 1)] != " ":
+            index_last_letter = - (x + 1)
+            break
 
-b = []
-print("numbers of matrix B: ")
-for i in range(rows_b):
-    row = list(map(int, input().split()))
-    b.append(row)
+    for x in range(len(string)):
+        if string[x] != " ":
+            index_first_letter = x
+            break
 
+    return string[index_first_letter:index_last_letter + 1]
 
-if cols_a != rows_b:
-    print("Error: Number of columns of matrix A should be equal to number of rows of matrix B")
-else:
-    result = [[0 for j in range(cols_b)] for i in range(rows_a)]
-
-for i in range(rows_a):
-    for j in range(cols_b):
-        for k in range(cols_a):
-            result[i][j] += a[i][k] * b[k][j]
-print(result)
-for row in result:
-    print(row)
 
 end = perf_counter()
 print(f"Program execution time: {end - start:.2f}")
